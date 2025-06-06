49ceff166dcc68d89dc80a66767e74fdd24ad70c
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2025-06-06 00:49:22 -0300

    4.11: Added beforeAll/Each and afterAll/Each test methods examples

--- jest-basics/__tests__/describe_it.spec.js
+++ jest-basics/__tests__/describe_it.spec.js
@@ -1,3 +1,24 @@
+beforeAll(() => {
+    console.log('beforeAll');
+});
+
+beforeAll(() => {
+    // You can repeat the method. Is called in the declared order
+    console.log('beforeAll repeated');
+});
+
+afterAll(() => {
+    console.log('afterAll')
+});
+
+beforeEach(() => {
+    console.log('beforeEach');
+});
+
+afterEach(() => {
+    console.log('afterEach')
+});
+
 // Group related tests
 describe('Auth', () => {
     // alias for test() in a more human-readable flavour
