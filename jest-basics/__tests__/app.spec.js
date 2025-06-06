d91027450fba34fe94c16f94a9e3a7a4ebec05c8
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2025-06-06 00:36:53 -0300

    4.9: Added jest.spyOn example

--- jest-basics/__tests__/app.spec.js
+++ jest-basics/__tests__/app.spec.js
@@ -3,10 +3,20 @@ const crypto = require('crypto');
 
 jest.mock('crypto');
 
-test('Fetch Data', async () => {
+test('Fetch Data (mock)', async () => {
+
     // mockResolvedValueOnce: fn injected by jest.mock
     crypto.randomBytes.mockResolvedValueOnce('bytes');
 
     const res = await getData();
-    console.log(res);
+    expect(res).toBe('bytes');
 });
+
+test('Fetch Data (spyOn)', async () => {
+    // mockResolvedValueOnce similar to mockImplementationOnce(() => Promise.resolve(...));
+    jest.spyOn(crypto, 'randomBytes')
+        .mockResolvedValueOnce('bytes');
+
+    const res = await getData();
+    expect(res).toBe('bytes');
+})
