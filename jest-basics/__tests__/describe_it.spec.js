095b6c4d80fa60a57f6bf4cde851576aa9beed3f
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2025-06-06 00:43:16 -0300

    4.10: Added describe() and it() examples

---
+++ jest-basics/__tests__/describe_it.spec.js
@@ -0,0 +1,12 @@
+// Group related tests
+describe('Auth', () => {
+    // alias for test() in a more human-readable flavour
+    it('Should run auth test 1', () => {});
+    it('Should run auth test 2', () => {});
+});
+
+describe('Products', () => {
+    // alias for test() in a more human-readable flavour
+    it('Should run products test 1', () => {});
+    it('Should run products test 2', () => {});
+});
